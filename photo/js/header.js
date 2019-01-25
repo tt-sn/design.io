dddbcdc084d4b6c558c0f3fae7a5e0a3b774ecb0
tt-sn <rb.totto@gmail.com>
2019-01-26 08:34:50 +0900

    page move update

--- photo/js/header.js
+++ photo/js/header.js
@@ -1,3 +1,7 @@
+(window.onload = function() {
+    $('#filter').fadeOut(3000);
+})();
+
 function cntBtn(){
     var cnt = document.getElementById("contact");
     if(cnt.style.display=="block"){
@@ -8,9 +12,18 @@ function cntBtn(){
         $('#medium').animate({'height':ht +"px"},"slow");
 	}else{
         // blockで表示
-        let ht = $('#medium').height();
+        var ht = $('#medium').height();
         ht += 80;
         $('#medium').animate({'height':ht +"px"},"slow");
         $('#contact').show();
 	}
 }
+
+function stdBtn(){
+    $.when(
+        $('#filter').fadeIn(3000),
+    ).done(function(){
+        alert("this");
+        document.location.href="//tt-sn.github.io/design.io/portofolio";
+    });
+  }
