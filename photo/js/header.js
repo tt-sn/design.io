550dafb8dacd93a07ffc28b9bc42d4803629081a
tt-sn <rb.totto@gmail.com>
2019-01-26 14:54:39 +0900

    photo update

--- photo/js/header.js
+++ photo/js/header.js
@@ -8,29 +8,36 @@ function cntBtn(){
         // noneで非表示
         $('#contact').hide("");
         let ht = $('#medium').height();
-        ht -= 80;
+        ht -= 60;
         $('#medium').animate({'height':ht +"px"},"slow");
 	}else{
         // blockで表示
         var ht = $('#medium').height();
-        ht += 80;
+        ht += 60;
         $('#medium').animate({'height':ht +"px"},"slow");
         $('#contact').show();
 	}
 }
+function abtBtn(){
+    $.when(
+        $('#filter').fadeIn(1500),
+    ).done(function(){
+        document.location.href="https://tt-sn.github.io/design.io/photo/about.html";
+    });
+  }
 
 function stdBtn(){
     $.when(
-        $('#filter').fadeIn(2000),
+        $('#filter').fadeIn(1500),
     ).done(function(){
         document.location.href="https://tt-sn.github.io/design.io/portfolio";
     });
   }
 
-  function abtBtn(){
+  function phtBtn(){
     $.when(
-        $('#filter').fadeIn(2000),
+        $('#filter').fadeIn(1500),
     ).done(function(){
-        document.location.href="https://tt-sn.github.io/design.io/photo/about.html";
+        document.location.href="https://tt-sn.github.io/design.io/photo/photo.html";
     });
   }
